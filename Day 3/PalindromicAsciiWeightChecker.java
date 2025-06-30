4607f2fdb1a543669993d5728a0e0aa611277d52
itsabbhii <0221csds100@gmail.com>
2025-06-30 11:52:42 +0530

    done

--- Day 3/PalindromicAsciiWeightChecker.java
+++ Day 3/PalindromicAsciiWeightChecker.java
@@ -13,7 +13,7 @@ public class PalindromicAsciiWeightChecker {
     static int sumOfAscii(String str){
         int sum = 0;
         for(int i = 0 ; i<str.length(); i++){
-            sum += (int)str.charAt(i);
+            sum += str.charAt(i);
         }
         return sum;
     }
