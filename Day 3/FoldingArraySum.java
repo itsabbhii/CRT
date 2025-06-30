1c44b8ac744122efbb3b9bda4fad5be7fa4eb2a6
itsabbhii <0221csds100@gmail.com>
2025-06-30 12:28:39 +0530

    day 3 wrapped

--- Day 3/FoldingArraySum.java
+++ Day 3/FoldingArraySum.java
@@ -7,7 +7,7 @@ public class FoldingArraySum {
             left++;
             right--;
         }
-        return newArr;
+        return newArr; 
     }
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5, 6, 8};
