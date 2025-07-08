29b25bbe6a0fd19b2f71f9c5da6002df9079c8d4
itsabbhii <0221csds100@gmail.com>
2025-07-08 11:10:08 +0530

    Day 10

--- Day 10/LoginExceptionExample.java
+++ Day 10/LoginExceptionExample.java
@@ -10,14 +10,31 @@ class UsernameException extends Exception{
     }
 }
 public class LoginExceptionExample {
+    static Map<String, String> map = new HashMap<>();
+    static {
+        map.put("admin", "1234");
+        map.put("abhinav", "abcd");
+        map.put("rahul", "1098");
+        map.put("aman", "1111");
+
+    }
     public static void main(String[] args) throws PasswordException, UsernameException{
         Scanner input = new Scanner(System.in);
         System.out.print("Enter username: ");
         String uname = input.next();
         System.out.print("Enter password: ");
         String pass = input.next();
-        if(!uname.equals("admin")) throw new UsernameException("Invalid user");
-        if(!pass.equals( "1234")) throw new PasswordException("Invalid password");
-        System.out.println("Login Successfull");
+        try {
+            login(uname, pass);
+        } catch (UsernameException e) {
+            System.out.println("UsernameException Occurred : "+e.getMessage());
+        }catch (PasswordException e){
+            System.out.println("PasswordException Occurred: "+e.getMessage());
+        }
+    }
+    static void login(String uname, String pass)throws UsernameException, PasswordException{
+        if(!map.containsKey(uname)) throw new UsernameException("Invalid username");
+        if(!map.get(uname).equals(pass)) throw new PasswordException("Invalid password");
+        System.out.println("Successful Login!!");
     }    
 }
